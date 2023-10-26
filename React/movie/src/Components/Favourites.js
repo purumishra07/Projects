3dfeb8fb477aa1be8f68d0696093ee80ad19310b
purumishra07 <purumishra07@gmail.com>
2023-10-27 00:52:55 +0530

    :bookmark: favourites component

--- React/movie/src/Components/Favourites.js
+++ React/movie/src/Components/Favourites.js
@@ -17,9 +17,11 @@ export default class Favourites extends Component {
     })
     }
     render() {
+        let genreId={28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
+        27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'}
         return (
             <div class="row">
-                <div class="col-3">
+                <div class="col-3 favourites-list">
                     <ul class="list-group">
                         <li class="list-group-item active" aria-current="true">All Genres</li>
                         <li class="list-group-item">Fantasy</li>
@@ -27,10 +29,10 @@ export default class Favourites extends Component {
                         <li class="list-group-item">Horror</li>
                     </ul>
                 </div>
-                <div class="col">
+                <div class="col favourites-table">
                     <div class="row">
-                        <input type="text" className="col" placeholder="Search"></input>
-                        <input type="number" className="col" placeholder="5"></input> 
+                        <input type="text" className="col-8" placeholder="Search"></input>
+                        <input type="number" className="col-4" placeholder="5"></input> 
                     </div>
                     <div class="row">
                     <table class="table">
@@ -47,24 +49,16 @@ export default class Favourites extends Component {
                         {
                             this.state.movies.map((movieObj)=>(
                             <tr>
-                                <td scope="row">{movieObj.original_title}</td>
-                                <td>{movieObj.original_title}</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
+                                <td scope="row"><img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} style={{width:'6rem'}}/>{movieObj.original_title}</td>
+                                <td>{genreId[movieObj.genre_ids[0]]}</td>
+                                <td>{movieObj.popularity}</td>
+                                <td>{movieObj.vote_average}</td>
+                                <td>
+                                    <button class="btn btn-outline-danger">Delete</button>
+                                </td>
                             </tr>
                             ))
                         }
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td colspan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
                         </tbody>
                     </table>
                     </div>
