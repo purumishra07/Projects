7779d20a6eaaa60657d547f05cece60dcf4a7527
purumishra07 <purumishra07@gmail.com>
2023-11-02 00:52:45 +0530

    :fire: project completed

--- React/movie/src/App.js
+++ React/movie/src/App.js
@@ -4,19 +4,26 @@ import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import List from './Components/List';
 import Favourites from './Components/Favourites';
+import React, { Component }  from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
   return (
-    <>
-    {/* <Navbar/>
-    <Banner/>
-    <List/> */}
-    <Navbar/>
-    <Favourites/>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <List />
+            </>
+          }
+        />
+        <Route path="/fav" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
--- React/movie/src/Components/Banner.js
+++ React/movie/src/Components/Banner.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { movies } from './getMovies'
+import { movies } from './getMovies';
 
 export default class Banner extends Component {
     render() {
